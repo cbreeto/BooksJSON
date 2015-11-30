092f66575df5093e8201368e5c76627f87757da5
Carlos <freakcompany@hotmail.com>
2015-11-29 23:48:12 -0500

    Inclusión de Alerts y Mejor lectura de Array

--- BooksJSON/ViewController.swift
+++ BooksJSON/ViewController.swift
@@ -23,49 +23,66 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     func textFieldShouldReturn(isbnField: UITextField) -> Bool {
         isbnTextField.resignFirstResponder()
-        
-        getInfoJSON(isbnField.text!)
-        
-        
+        if isbnTextField.text != "" {
+            getInfoJSON(isbnField.text!)
+        }
+        else {
+            emptyField()
+        }
         return true
-        
+    }
+    
+    func emptyField() -> Void {
+        let alerta = UIAlertController(title: "Incompleto", message: "Falta introducir ISBN a buscar", preferredStyle: .Alert)
+        alerta.addAction(UIAlertAction(title: "Ok", style: .Default, handler: nil))
+        self.presentViewController(alerta, animated: true, completion: nil)
     }
     
     func getInfoJSON(id : String) -> Void {
         
         let isbn = self.isbnTextField.text
         let urls = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:" + isbn!
-        let id = "ISBN" + isbn!
+        let id = "ISBN:" + isbn!
         
         
         let url = NSURL(string: urls)
         
-        let datos = NSData(contentsOfURL: url!)
+        let datos: NSData? = NSData(contentsOfURL: url!)
         
 
         
         do {
             let json = try NSJSONSerialization.JSONObjectWithData(datos!, options: NSJSONReadingOptions.MutableLeaves)
-        
+            print(id)
             let dict1 = json as! NSDictionary
             let dict2 = dict1[id] as! NSDictionary
-            
             tituloLbl.text = dict2["title"] as! NSString as String
-            let cadena : String = dict2["by_statement"] as! NSString as String
-            let until : Character = ";"
             
-            let author : String = (cadena.substringToIndex(cadena.characters.indexOf(
-                until)!))
+            //Otra forma de obtener autor
+            //let cadena : String = dict2["by_statement"] as! NSString as String
+            //let until : Character = ";"
             
-            autor1Lbl.text = author
+            //let author : String = (cadena.substringToIndex(cadena.characters.indexOf(
+            //    until)!))
+            
+            let dict3 = dict2["authors"] as! NSArray
+            dict3.enumerateObjectsUsingBlock({ objeto, index, stop in
+                let dict4 = objeto["name"] as! String
+                self.autor1Lbl.text = dict4
+            })
+            
+            let dict5 = dict2["cover"] as? NSDictionary
+            if dict5 == nil {
+                let alerta = UIAlertController(title: "Alerta", message: "No hay imagen de cover", preferredStyle: .Alert)
+                alerta.addAction(UIAlertAction(title: "Ok", style: .Default, handler: nil))
+                self.presentViewController(alerta, animated: true, completion: nil)
+            } else {
+                pagesLbl.text = "El libro si cuenta con cover"
+            }
             pagesLbl.text = dict2["notes"] as! NSString as String
             
-            
-            //  autor1Lbl.text = dict3[1] as!
-            //     NSString as String  No se puede convertir un NSDictionary a un NSString. Busque en muchos lados pero no me salió
-            
         }catch _ {
-            
+            print("Error en JSONSerializacion")
         }
         
     }
